f7641fff9f7cfd1bf0fa970e20b04cae9e36f834
Samiksha-lync <Samiksha@lync.world>
2024-07-31 11:06:18 +0530

    final commit

--- src/components/Coins/Coininfo.js
+++ src/components/Coins/Coininfo.js
@@ -27,8 +27,10 @@ function Coininfo() {
     fetchCoinData();
   }, [id]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (loading)
+     return <div>Loading...</div>;
+  if (error) 
+    return <div>Error: {error.message}</div>;
 
   return (
     <div className='coin-data'>
